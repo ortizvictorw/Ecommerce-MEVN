da452d7509f3dd57df4bd8be6bac5afe59d1ae6b
Francisco Toyos <francabj95@gmail.com>
2021-01-18 15:29:19 -0300

    Se realizó el index, falta mostrar imagen en front y terminar cards con su respectiva imagen default

---
+++ Back-end/src/app/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './components/Index.vue';
+
+
+new Vue({
+    render: m => m(App)
+}).$mount('#app')
--- Back-end/src/database/config.js
+++ Back-end/src/database/config.js
@@ -1,7 +1,7 @@
 //** Coneccion a la base de Datos Mongodb **/
 const mongoose = require ('mongoose');
-const coneccionDB = ()=>{
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.svigf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
+const coneccionDB = () => {
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7fmuk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
         useNewUrlParser:true,
         useUnifiedTopology:true
     })
