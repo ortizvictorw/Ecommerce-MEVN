bf52461ddee6cce3a7d6ea45a8fb1f5849db3830
ORTIZVICTORW <ortiz.victor.w@gmail.com>
2021-01-22 11:43:10 -0300

    endpoin.todo agregado2

--- Back-end/src/routes/usuarios.js
+++ Back-end/src/routes/usuarios.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const usuariosController = require ('../controllers/usuariosController')
 const { verificacionToken , esAdministrador ,validacionDeRoles} = require('../middlewares/index')
 /* GET a los usuarios -- endpoint:http://localhost:3000/api/usuarios */
-router.get('/', usuariosController.listar);
+router.get('/',[verificacionToken,esAdministrador], usuariosController.listar);
 
 /* POST a los usuarios -- endpoint:http://localhost:3000/api/usuarios */
-router.post('/', usuariosController.register);
+router.post('/',[verificacionToken,esAdministrador], usuariosController.register);
 
 /* PUT al usuario -- endpoint:http://localhost:3000/api/usuarios */
 router.put('/:id',[verificacionToken,esAdministrador], usuariosController.editar);
 
 /* DELETE al usuario -- endpoint:http://localhost:3000/api/usuarios */
-router.delete('/:id', usuariosController.eliminar);
+router.delete('/:id',[verificacionToken,esAdministrador], usuariosController.eliminar);
 
 /* login del usuario -- endpoint:http://localhost:3000/api/usuarios/login */
 router.post('/login',verificacionToken, usuariosController.login);
