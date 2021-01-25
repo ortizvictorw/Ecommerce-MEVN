bc19b35b92f2533cc6e3529e10f05a5263d89538
ORTIZVICTORW <ortiz.victor.w@gmail.com>
2021-01-25 11:56:31 -0300

    FrontVue

---
+++ Front-end/mini-ecommerce-front/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="DesarrollaWeb logo" src="./assets/logo.png">
+  <Login />
+</template>
+
+<script>
+import Login from './components/Login'
+
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+img{
+  max-height: 200px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
