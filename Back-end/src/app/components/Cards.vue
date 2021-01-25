f97c3c8f770c08adf3b935c4e623aa324b370c7d
Francisco Toyos <francabj95@gmail.com>
2021-01-25 03:13:45 -0300

    Api consumida con axios

--- Back-end/src/app/components/Cards.vue
+++ Back-end/src/app/components/Cards.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="row container mx-auto">
-    <div v-for="(producto,index) in productos" :key="index">
+    <div v-for="(producto,i) in productos" :key="i">
       <div class="card col-12 my-1 mx-2">
-        <img :src="producto.src" class="card-img-top" :alt="producto.titulo" />
+        <img :src="producto.imagen" class="card-img-top" :alt="producto.titulo" />
         <div class="card-body">
           <h5 class="card-title">{{ producto.titulo }}</h5>
           <h6>${{producto.precio}}</h6>
-          <p class="card-text">Excelente opcion para cualquier ocasion</p>
+          <p class="card-text"> {{producto.descripcion}} </p>
           <a href="#" class="btn btn-primary">Agregar a Carrito</a>
         </div>
       </div>
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Cards",
   props:{
@@ -22,38 +23,22 @@ export default {
   },
   data() {
     return {
-      productos: [
-        {
-          titulo: "Remera blanca de dama",
-          src:
-            "https://files.cdn.printful.com/o/upload/variant-image/12/123eded87f4c54cee78f7c299e517fd4_l",
-          precio:1000,
-        },
-        {
-          titulo: "Remera blanca de caballero",
-          src:
-            "https://files.cdn.printful.com/o/upload/variant-image/2e/2ecfce77feb3657d328119783492dc5e_l",
-            precio:900,
-        },
-        {
-          titulo: "Canguro blanco de caballero",
-          src:
-            "https://files.cdn.printful.com/o/upload/variant-image/40/40a573d12e9fa5f5c0228d132bfd7f07_l",
-            precio:2500,
-        },
-        {
-          titulo: "Pantalon deportivo negro de caballero",
-          src:
-            "https://files.cdn.printful.com/o/upload/variant-image-jpg/89/89140b7773b9183565e79267508221e7_l",
-            precio:2000,
-        },
-        {
-          titulo: "Producto sin imagen",
-          src: "https://kubalubra.is/wp-content/uploads/2017/11/default-thumbnail.jpg",
-          precio:0,
-        },
-      ],
+      productos: [],
     };
+  },
+  mounted() {
+    console.log('ciclo montado')
+    this.leerApi();
+  },
+  methods: {
+    leerApi() {
+      axios
+        .get("https://mini-ecommerce-api.herokuapp.com/api/productos")
+        .then(res => {
+          this.productos = res.data
+        })
+        .catch(e => console.log(e))
+    }
   },
 };
 </script>
